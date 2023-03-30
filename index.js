83c301f1ef41636cdc64764ff2eb2d86d0eb3851
sowmik-sec <sowmik.sec@gmail.com>
2023-03-30 10:59:29 +0600

    initializing express server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send(`doctor's portal server is running`);
+});
+
+app.listen(port, () => console.log(`Doctor's portal running on port ${port}`));
