2a4b93381346a50568f71f8294dc315f6e516591
sowmik-sec <sowmik.sec@gmail.com>
2023-03-31 11:42:02 +0600

    creating booking api

--- index.js
+++ index.js
@@ -95,6 +95,12 @@ const run = async () => {
         .toArray();
       res.send(options);
     });
+    app.get("/bookings", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    });
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       console.log(booking);
