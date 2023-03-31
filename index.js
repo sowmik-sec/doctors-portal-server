ce567367144faee56c297d94f3e6b64adf56904c
sowmik-sec <sowmik.sec@gmail.com>
2023-03-31 17:36:36 +0600

    simple JWT token

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -118,6 +119,18 @@ const run = async () => {
       }
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
+    });
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
     });
     app.post("/users", async (req, res) => {
       const user = req.body;
